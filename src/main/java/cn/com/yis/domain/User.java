d0e6459206516e50ff67680ab4e7749e130aa452
yis <yis@nes.com.cn>
2025-10-12 00:28:02 +0800

    属性注入-数据校验。

--- src/main/java/cn/com/yis/domain/User.java
+++ src/main/java/cn/com/yis/domain/User.java
@@ -1,16 +1,20 @@
 package cn.com.yis.domain;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Data
 @Component
 @ConfigurationProperties(prefix = "user")
+@Validated
 public class User {
     private String name;
     private Integer age;
     private String phone;
+    @NotBlank(message = "邮箱不能为空")
     private String email;
 
     private Address address;
